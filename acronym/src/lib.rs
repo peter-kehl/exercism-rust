690a8f276f1b14ad5bda55dc7027e416fe061e6a
Peter Kehl <peter.kehl@gmail.com>
2022-02-17 07:33:30 -0800

    State machine

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,31 @@
+#[derive(PartialEq, PartialOrd)]
+enum ParserState {
+    WordStart,
+    UpperCase,
+    LowerCase,
+}
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    phrase
+        .chars()
+        .fold(
+            (ParserState::WordStart, "".to_owned()),
+            |(previous, subresult), c| {
+                assert!(subresult != "" || previous == ParserState::WordStart);
+                if c.is_whitespace() || c == '-' || c == '_' {
+                    return (ParserState::WordStart, subresult);
+                }
+
+                match (previous, c.is_uppercase()) {
+                    (ParserState::WordStart, _) => (ParserState::UpperCase, format!("{subresult}{c}", c = c.to_uppercase())),
+                    (ParserState::UpperCase, true) => (ParserState::UpperCase, subresult),
+                    (ParserState::UpperCase, false) => (ParserState::LowerCase, subresult),
+                    
+                    (ParserState::LowerCase, false) => (ParserState::LowerCase, subresult),
+                    (ParserState::LowerCase, true) => (ParserState::UpperCase, format!("{subresult}{c}")),
+                }
+            },
+        )
+        .1
+        .to_owned()
 }
